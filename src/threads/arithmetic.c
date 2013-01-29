ae09205db8309e5a8fb8f83f567693bb0b7d7f74
Owen Davies <tod11@doc.ic.ac.uk>
2013-01-29 15:24:08 +0000

    Not quite sure if my approach is correct, but I think this is all we need. Added a typedef to make things clearer

--- src/threads/arithmetic.c
+++ src/threads/arithmetic.c
@@ -1,4 +1,5 @@
 #include "arithmetic.h"
+#include <stdio.h>
 
 /* Implementation of Fixed-Point Real Arithmetic operations as seen in
    Appendix B.6 of the PintOS specification.
@@ -7,19 +8,19 @@
    calculations like recent_cpu and load_avg */
 
 /* Convert an integer to a fixed point number */
-int32_t int_to_fp (int32_t i)
+fixed_point_t int_to_fp (int32_t i)
 {
     return i * F;
 }
 
 /* Convert an fixed point number to an integer, rounding down to 0 always */
-int32_t fp_to_int_rt0 (int32_t x)
+int32_t fp_to_int_rt0 (fixed_point_t x)
 {
     return x / F;
 }
 
 /* Convert fixed point to integer, rounding to nearest */
-int32_t fp_to_int_rtn (int32_t x)
+fixed_point_t fp_to_int_rtn (fixed_point_t x)
 {
     if (x > 0)
         return (x + F / 2) / F;
@@ -28,49 +29,49 @@ int32_t fp_to_int_rtn (int32_t x)
 }
 
 /* Sum two fixed point numbers */
-int32_t sum_two_fps (int32_t x, int32_t y)
+fixed_point_t sum_two_fps (fixed_point_t x, fixed_point_t y)
 {
     return x + y;
 }
 
 /* Sum an integer I with a fixed point number FP */
-int32_t sum_int_fp (int32_t i, int32_t fp)
+fixed_point_t sum_int_fp (int32_t i, fixed_point_t fp)
 {
     return fp + i * F;
 }
 
 /* Subtract fixed point Y from fixed point X */
-int32_t diff_two_fps (int32_t x, int32_t y)
+fixed_point_t diff_two_fps (fixed_point_t x, fixed_point_t y)
 {
     return x - y;
 }
 
 /* Subtract integer I from fixed point FP */
-int32_t diff_int_fp (int32_t i, int32_t fp)
+fixed_point_t diff_int_fp (int32_t i, fixed_point_t fp)
 {
     return fp - i * F;
 }
 
 /* Multiply two fixed point numbers */
-int32_t mul_two_fps (int32_t x, int32_t y)
+fixed_point_t mul_two_fps (fixed_point_t x, fixed_point_t y)
 {
     return ((int64_t) x) * y / F;
 }
 
 /* Multiply an integer I by a fixed point number FP */
-int32_t mul_int_fp (int32_t i, int32_t fp)
+fixed_point_t mul_int_fp (int32_t i, fixed_point_t fp)
 {
     return i * fp;
 }
 
 /* Divide fp number X by fp number Y */
-int32_t div_two_fps (int32_t x, int32_t y)
+fixed_point_t div_two_fps (fixed_point_t x, fixed_point_t y)
 {
     return ((int64_t) x) * F / y;
 }
 
 /* Divide a floating point number FP by an integer I */
-int32_t div_int_fp (int32_t i, int32_t fp)
+fixed_point_t div_int_fp (int32_t i, fixed_point_t fp)
 {
     return fp / i;
 }
--- src/threads/arithmetic.h
+++ src/threads/arithmetic.h
@@ -6,26 +6,27 @@
 /* These operations are implemented using a p.q fixed point format. I have 
    chosen p = 17, q = 14 as in the specification. F is just 1 << Q */
 
-#define P 17
 #define Q 14
-#define F 16384
+#define F (1 << Q)
+
+typedef int32_t fixed_point_t;
 
 /* Conversion methods */
-int32_t int_to_fp (int32_t i);
-int32_t fp_to_int_rt0 (int32_t x);
-int32_t fp_to_int_rtn (int32_t x);
+fixed_point_t int_to_fp (int32_t i);
+int32_t fp_to_int_rt0 (fixed_point_t x);
+int32_t fp_to_int_rtn (fixed_point_t x);
 
 /* Arithmetic operations */
-int32_t sum_two_fps (int32_t x, int32_t y);
-int32_t sum_int_fp (int32_t i, int32_t fp);
+fixed_point_t sum_two_fps (fixed_point_t x, fixed_point_t y);
+fixed_point_t sum_int_fp (int32_t i, fixed_point_t fp);
 
-int32_t diff_two_fps (int32_t x, int32_t y);
-int32_t diff_int_fp (int32_t i, int32_t fp);
+fixed_point_t diff_two_fps (fixed_point_t x, fixed_point_t y);
+fixed_point_t diff_int_fp (int32_t i, fixed_point_t fp);
 
-int32_t mul_two_fps (int32_t x, int32_t y);
-int32_t mul_int_fp (int32_t i, int32_t fp);
+fixed_point_t mul_two_fps (fixed_point_t x, fixed_point_t y);
+fixed_point_t mul_int_fp (int32_t i, fixed_point_t fp);
 
-int32_t div_two_fps (int32_t x, int32_t y);
-int32_t div_int_fp (int32_t i, int32_t fp);
+fixed_point_t div_two_fps (fixed_point_t x, fixed_point_t y);
+fixed_point_t div_int_fp (int32_t i, fixed_point_t fp);
 
 #endif
