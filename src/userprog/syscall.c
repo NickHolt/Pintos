f9e90fa9aaa16ed9c60c190b65d433a55d8daaf0
Owen Davies <tod11@doc.ic.ac.uk>
2013-02-12 15:13:08 -0800

    Started work on exit system call, maybe it's done but I think we may need parent stuff in there

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,11 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
+static void halt (void);
+static void exit (int status);
 
 void
 syscall_init (void)
@@ -27,11 +30,11 @@ syscall_handler (struct intr_frame *f)
   switch (syscall_number)
     {
       case SYS_HALT:
-        /* call halt() */
+        halt ();
         break;
 
       case SYS_EXIT:
-        /* call exit() */
+        exit (*(stack_pointer + 1));
         break;
 
       case SYS_WRITE:
@@ -50,4 +53,18 @@ halt (void)
 {
   // I think this is all we need here?
   shutdown_power_off ();
+}
+
+/* The exit system call. Terminate the current user program, sending the exit
+   status to the kernel */
+static void
+exit (int status)
+{
+  struct thread *exiting_thread = thread_current();
+  printf("%s: exit(%d)\n", exiting_thread->name, status);
+
+  // I think there might need to be stuff to do with parents here, which will
+  // involve adding a list of children to the thread struct probably
+
+  thread_exit();
 }
