02721d8f605493768fc92667a32a9a5a8f797905
Owen Davies <tod11@doc.ic.ac.uk>
2013-02-12 13:22:47 -0800

    Started work on syscall handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,14 +7,39 @@
 static void syscall_handler (struct intr_frame *);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+/* Switch on the system call numbers defined in lib/syscall-nr.h, and call
+   the appropriate system call */
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int *stack_pointer = f->esp;
+
+  /* TODO: check if pointer is valid */
+
+  int syscall_number = *stack_pointer;
+
+  // Maybe function pointers would be neater here?
+  switch (syscall_number)
+    {
+      case SYS_HALT:
+        /* call halt() */
+        break;
+
+      case SYS_EXIT:
+        /* call exit() */
+        break;
+
+      case SYS_WRITE:
+        /* call write() */
+        break;
+
+      case SYS_WAIT:
+        /* call wait() */
+        break;
+    }
 }
