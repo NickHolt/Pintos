e285e363e921fd4edb7c440efa17aa786cbef996
Charlie Hothersall-Thomas <cjh111@doc.ic.ac.uk>
2013-02-19 15:17:43 +0000

    Add temporary change in setup_stack until argument passing is implemented.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -437,7 +437,8 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        *esp = PHYS_BASE - 12; /* -12 is temporary until argument passing is
+                                  implemented */
       else
         palloc_free_page (kpage);
     }
